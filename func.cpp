6c3a598531d09af4cb384d61fbb592a2536d88d2
admin <nazik@nazik.myguest.virtualbox.org>
2024-12-22 03:07:49 +0200

    Finish implementation, add comments

--- func.cpp
+++ func.cpp
@@ -1,6 +1,8 @@
-#include "func.h"
-
-double Func::FuncA() {
-    return 0;
+double Func::FuncA(int n) {
+    double result = 0;
+    for (int i = 1; i <= n; ++i) {
+        result += pow(-1, i + 1) * pow(4, 2 * i - 1) / (2 * i - 1);
+    }
+    return result;
 }
-
+// n: number of terms to sum in the infinite sequence
